91ab0acfb60d25196231e221957acaf7bbe560ec
Vinicius Gouvêa de Moura <gouveamoura@prof.educacao.sp.gov.br>
2024-10-05 12:14:45 -0300

    Update 02_heranca_multipla.py

--- 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
@@ -1,18 +1,18 @@
-class Animal:
-    def __init__(self, nro_patas):
-        self.nro_patas = nro_patas
+class Animal:#criar classe animal
+    def __init__(self, nro_patas): # construtor número de patas
+        self.nro_patas = nro_patas 
 
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 
-class Mamifero(Animal):
+class Mamifero(Animal):#criar classe mamifero filho de animal
     def __init__(self, cor_pelo, **kw):
-        self.cor_pelo = cor_pelo
+        self.cor_pelo = cor_pelo ## construtor cor do pelo
         super().__init__(**kw)
 
 
-class Ave(Animal):
+class Ave(Animal): #criar classe ave filho de animal
     def __init__(self, cor_bico, **kw):
         self.cor_bico = cor_bico
         super().__init__(**kw)
